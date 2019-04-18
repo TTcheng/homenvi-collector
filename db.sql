897cc2db4776493812c273f1cc5962b02c295c5b
王春成 <ttchengwang@foxmail.com>
2019-04-18 17:23:44 +0800

    [FIX] 修改数据库脚本

--- db.sql
+++ db.sql
@@ -6,15 +6,8 @@ GRANT WRITE ON homenvi TO "homenvi-collector";
 
 -- shell: influx -username homenvi-collector -password xxxx
 USE homenvi;
-INSERT humiture,identifier=HomenviCollectorAlpha humidity=66.40,celsius=22.60,fahrenheit=72.68,heatIndexCelsius=22.65,heatIndexFahrenheit=72.77
+INSERT collections,identifier=HomenviCollectorAlpha humidity=66.40,celsius=22.60,fahrenheit=72.68,heatIndexCelsius=22.65,heatIndexFahrenheit=72.77
 -- 数据保存策略（Retention Policies）
 CREATE RETENTION POLICY "homenvi_rp" ON "homenvi" DURATION 30d REPLICATION 1 DEFAULT
 -- 连续查询（continuous queries）
-CREATE CONTINUOUS QUERY humiture_cq_1h ON homenvi \
-BEGIN \
-    SELECT identifier,mean(humidity),mean(celsius),mean(fahrenheit),mean(heatIndexCelsius),mean(heatIndexFahrenheit) \
-    INTO humiture_1h \
-    FROM humiture \
-    
-    GROUP BY identifier,time(1h) \
-END
+CREATE CONTINUOUS QUERY collections_cq_1h ON homenvi BEGIN SELECT identifier,mean(humidity),mean(celsius),mean(fahrenheit),mean(heatIndexCelsius),mean(heatIndexFahrenheit) INTO collections_1h FROM collections GROUP BY identifier,time(1h) END
